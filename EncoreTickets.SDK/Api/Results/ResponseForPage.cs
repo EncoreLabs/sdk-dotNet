7c09429e878183e6076633741c63d4b3c5144a1b
Tatyana Evtushenko <tevtushenko@encore.co.uk>
2019-10-29 17:38:17 +0300

    PUM-3028 Add a call to the pricing service in order to receive exchange rates.

---
+++ EncoreTickets.SDK/Api/Results/ResponseForPage.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using EncoreTickets.SDK.Interfaces;
+
+namespace EncoreTickets.SDK.Api.Results
+{
+    /// <summary>
+    /// The entity for collecting items received with additional information for pagination.
+    /// </summary>
+    /// <typeparam name="T">Model type</typeparam>
+    public class ResponseForPage<T> : IEnumerable<T>
+        where T : IObject
+    {
+        /// <summary>
+        /// Gets or sets the collection of received items.
+        /// </summary>
+        public List<T> items { get; set; }
+
+        /// <summary>
+        /// Gets or sets the page number.
+        /// </summary>
+        public int currentPage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of all existing pages.
+        /// </summary>
+        public int pageCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of items on this pages.
+        /// </summary>
+        public int itemsPerPage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of all existing items.
+        /// </summary>
+        public int totalItemCount { get; set; }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// An enumerator that can be used to iterate through the collection.
+        /// </returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            return items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ EncoreTickets.SDK/Pricing/Models/ExchangeRate.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using EncoreTickets.SDK.Interfaces;
+
+namespace EncoreTickets.SDK.Pricing.Models
+{
+    /// <summary>
+    /// Exchange rate DTO
+    /// </summary>
+    public class ExchangeRate : IObject
+    {
+        /// <summary>
+        /// Gets or sets an internal API ID.
+        /// </summary>
+        public int id { get; }
+
+        /// <summary>
+        /// Gets or sets a code of the source currency.
+        /// </summary>
+        public string baseCurrency { get; set; }
+
+        /// <summary>
+        /// Gets or sets a code of the target currency.
+        /// </summary>
+        public string targetCurrency { get; set; }
+
+        /// <summary>
+        /// Gets or sets the rate without margin.
+        /// </summary>
+        public decimal rate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the rate with margin.
+        /// encoreFxRate = fxRate * protection margin
+        /// </summary>
+        public decimal encoreRate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the protection margin.
+        /// This is in percentages.
+        /// </summary>
+        public int protectionMargin { get; set; }
+
+        /// <summary>
+        /// Gets or sets the time for the current exchange rate.
+        /// </summary>
+        public DateTime datetimeOfSourcing { get; set; }
+
+        public DateTime createdAt { get; set; }
+
+        public DateTime updatedAt { get; set; }
+
+        public int sourced { get; set; }
+    }
+}
