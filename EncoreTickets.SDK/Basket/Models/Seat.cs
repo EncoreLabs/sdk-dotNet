816d8ac2199a32cf6606f8da07718c3ecd77715c
Yury Struchkou <YStruchkov@encore.co.uk>
2019-11-11 14:50:00 +0300

    APS-1817 Added models for basket details call

---
+++ EncoreTickets.SDK/Basket/Models/Seat.cs
@@ -0,0 +1,17 @@
+﻿namespace EncoreTickets.SDK.Basket.Models
+{
+    public class Seat
+    {
+        public string aggregateReference { get; set; }
+
+        public string blockId { get; set; }
+
+        public string blockName { get; set; }
+
+        public string row { get; set; }
+
+        public string number { get; set; }
+
+        public string locationDescription { get; set; }
+    }
+}
