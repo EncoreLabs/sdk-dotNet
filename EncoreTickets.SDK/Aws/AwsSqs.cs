5435152bc41b259ce8774e9d5152e346ade08153
ystruchkov <ystruchkov@encore.co.uk>
2019-11-22 16:15:10 +0000

    TIM-3362 Added comments to public methods

--- EncoreTickets.SDK/Aws/AwsSqs.cs
+++ EncoreTickets.SDK/Aws/AwsSqs.cs
@@ -7,21 +7,39 @@ using Amazon.SQS.Model;
 
 namespace EncoreTickets.SDK.Aws
 {
+    /// <inheritdoc cref="IAwsSqs"/>
+    /// <summary>
+    /// The default implementation of the IAwsSqs interface.
+    /// </summary>
     public class AwsSqs : IAwsSqs
     {
         private readonly IAmazonSQS client;
 
+        /// <summary>
+        /// Constructor that instantiates this class by the name of an AWS profile and the name of an AWS region.
+        /// </summary>
+        /// <param name="profileName">The AWS profile name.</param>
+        /// <param name="regionName">The requested AWS region name.</param>
         public AwsSqs(string profileName, string regionName)
         {
             client = CreateClient(profileName, regionName);
         }
 
+        /// <summary>
+        /// Constructor that instantiates this class by the name of an AWS profile and the name of an AWS region.
+        /// The specified AWS profile is created or updated with the specified credentials.
+        /// </summary>
+        /// <param name="profileName">The AWS profile name.</param>
+        /// <param name="regionName">The requested AWS region name.</param>
+        /// <param name="accessKey">The AWS access key to set to the profile.</param>
+        /// <param name="secretKey">The AWS secret key to set to the profile.</param>
         public AwsSqs(string profileName, string regionName, string accessKey, string secretKey)
         {
             RegisterProfile(profileName, accessKey, secretKey);
             client = CreateClient(profileName, regionName);
         }
 
+        /// <inheritdoc cref="IAwsSqs"/>
         public async Task<SendMessageResponse> SendMessageAsync(string queueUrl, string messageBody)
         {
             var sqsRequest = new SendMessageRequest
