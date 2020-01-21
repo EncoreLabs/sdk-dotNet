e379bb03f5558bfc823cb4cca36d6512570c321a
Tatyana Evtushenko <tevtushenko@encore.co.uk>
2020-01-21 10:27:30 +0300

    APS-2079 Add unit tests for the enumerable extension.

--- EncoreTickets.SDK/Utilities/BaseTypesExtensions/EnumerableExtension.cs
+++ EncoreTickets.SDK/Utilities/BaseTypesExtensions/EnumerableExtension.cs
@@ -16,11 +16,22 @@ namespace EncoreTickets.SDK.Utilities.BaseTypesExtensions
         public static IEnumerable<T> DistinctBy<T>(this IEnumerable<T> source, Func<T, object> selector)
             => source?.GroupBy(selector).DefaultIfEmpty().Where(g => g != null).Select(g => g.FirstOrDefault());
 
+        /// <summary>
+        /// Returns new collection where there are no null or empty strings. 
+        /// </summary>
+        /// <param name="stringCollection">Source string collection.</param>
+        /// <returns>Enumerable without empty strings.</returns>
         public static IEnumerable<string> ExcludeEmptyStrings(this IEnumerable<string> stringCollection)
         {
             return stringCollection?.Where(x => !string.IsNullOrEmpty(x));
         }
 
+        /// <summary>
+        /// Returns null if source enumerable has no elements, otherwise returns the source enumerable. 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="enumerable">Source enumerable.</param>
+        /// <returns>Null or the source enumerable</returns>
         public static List<T> NullIfEmptyEnumerable<T>(this IEnumerable<T> enumerable)
         {
             return enumerable == null || !enumerable.Any() ? null : enumerable.ToList();
