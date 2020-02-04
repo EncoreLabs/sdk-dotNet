ae351ca840db70e39767025d36dadf501bdc013f
Tatyana Evtushenko <tevtushenko@encore.co.uk>
2020-02-04 18:05:31 +0300

    APS-2145 Add tests to test the Assert extension, which is often used

---
+++ EncoreTickets.SDK.Tests/Helpers/ObjectWithPropertiesOfBasicTypes.cs
@@ -0,0 +1,13 @@
+﻿namespace EncoreTickets.SDK.Tests.Helpers
+{
+    public class ObjectWithPropertiesOfBasicTypes
+    {
+        public string String { get; set; }
+
+        public int Int { get; set; }
+
+        public double Double { get; set; }
+
+        public bool Bool { get; set; }
+    }
+}
