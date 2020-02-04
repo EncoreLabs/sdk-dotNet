bc259f37f75efc9c917bc96d1ae025b8544d5069
Tatyana Evtushenko <tevtushenko@encore.co.uk>
2020-02-04 18:53:29 +0300

    APS-2145 Add integration tests to check order methods of the payment service

---
+++ EncoreTickets.SDK.Tests/IntegrationTests/PaymentServiceTests.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using EncoreTickets.SDK.Api.Models;
+using EncoreTickets.SDK.Api.Results.Exceptions;
+using EncoreTickets.SDK.Payment;
+using EncoreTickets.SDK.Payment.Models;
+using EncoreTickets.SDK.Payment.Models.RequestModels;
+using EncoreTickets.SDK.Tests.Helpers;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+
+namespace EncoreTickets.SDK.Tests.IntegrationTests
+{
+    internal class PaymentServiceTests
+    {
+        private IConfiguration configuration;
+        private Environments environment;
+        private ApiContext context;
+        private PaymentServiceApi service;
+        private string username;
+        private string password;
+        private string apiKey;
+
+        [SetUp]
+        public void SetupState()
+        {
+            configuration = ConfigurationHelper.GetConfiguration();
+            environment = Environments.Sandbox;
+            username = configuration["Payment:Username"];
+            password = configuration["Payment:Password"];
+            apiKey = configuration["Payment:ApiKey"];
+            context = new ApiContext(environment, apiKey);
+            service = new PaymentServiceApi(context, true);
+        }
+
+        [Test]
+        public void Authentication_IfUsingUsernameAndPassword_Successful()
+        {
+            context = new ApiContext(environment, username, password);
+            service = new PaymentServiceApi(context, true);
+
+            service.AuthenticationService.Authenticate();
+
+            Assert.True(service.AuthenticationService.IsThereAuthentication());
+        }
+
+        [Test]
+        public void GetOrder_Successful()
+        {
+            var channelId = configuration["Payment:TestChannelId"];
+            var externalId = configuration["Payment:TestOrderExternalId"];
+
+            var order = service.GetOrder(channelId, externalId);
+
+            AssertThatOrderIsCorrect(order, channelId, externalId);
+        }
+
+        [Test]
+        public void GetOrder_IfOrderWithChannelAndExternalIdDoesNotExist_Exception404()
+        {
+            var channelId = "not_channel";
+            var externalId = "not_id";
+
+            var exception = Assert.Catch<ApiException>(() =>
+            {
+                var order = service.GetOrder(channelId, externalId);
+            });
+
+            Assert.AreEqual(HttpStatusCode.NotFound, exception.ResponseCode);
+        }
+
+        [Test]
+        public void CreateOrder_Successful()
+        {
+            var channelId = configuration["Payment:TestChannelId"];
+            var externalId = configuration["Payment:TestCreateOrderExternalId"];
+            var orderRequest = new CreateOrderRequest
+            {
+                Description = "test description",
+                ChannelId = channelId,
+                ExternalId = externalId,
+                RedirectUrl = "https://payment-service.qatixuk.io/redirect",
+                Origin = "https://payment-service.qatixuk.io",
+                Amount = new Amount
+                {
+                    Value = 8100,
+                    Currency = "USD",
+                    ExchangeRate = 1.2
+                },
+                AmountOriginal = new Amount
+                {
+                    Value = 7000,
+                    Currency = "USD",
+                    ExchangeRate = 1.2
+                },
+                BillingAddress = new Address
+                {
+                    Line1 = "115 Shaftesbury Avenue",
+                    Line2 = null,
+                    PostalCode = "WC2H 8AF",
+                    City = "Cambridge Circus",
+                    CountryCode = "UK",
+                    LegacyCountryCode = null,
+                    StateOrProvince = "London"
+                },
+                Shopper = new Shopper
+                {
+                    Email = "test@test.com",
+                    TelephoneNumber = "02072578183",
+                    Title = "MS",
+                    FirstName = "INNA",
+                    LastName = "IVANOVA",
+                    ExternalId = null
+                },
+                Items = new List<OrderItem>
+                {
+                    new OrderItem
+                    {
+                        Name = "WICKED",
+                        Description = null,
+                        Quantity = 1,
+                        Amount = new Amount
+                        {
+                            Value = 8100,
+                            Currency = "USD",
+                            ExchangeRate = 1.2
+                        },
+                        Tax = null,
+                        ExternalId = "1587"
+                    }
+                },
+                RiskData = new RiskData
+                {
+                    DaysToEvent = 0,
+                    DeliveryMethod = "collection",
+                    OfficeId = 1
+                }
+            };
+
+            var order = service.CreateOrder(orderRequest);
+
+            AssertThatOrderIsCorrect(order, channelId, externalId);
+        }
+
+        [Test]
+        public void CreateOrder_IfOrderExists_Exception400()
+        {
+            var channelId = configuration["Payment:TestChannelId"];
+            var externalId = configuration["Payment:TestCreateOrderExternalId"];
+            var orderRequest = new CreateOrderRequest
+            {
+                Description = "test description",
+                ChannelId = channelId,
+                ExternalId = externalId,
+                RedirectUrl = "https://payment-service.qatixuk.io/redirect",
+                Origin = "https://payment-service.qatixuk.io",
+                Amount = new Amount
+                {
+                    Value = 8100,
+                    Currency = "USD",
+                    ExchangeRate = 1.2
+                },
+            };
+
+            var exception = Assert.Catch<ApiException>(() =>
+            {
+                var order = service.CreateOrder(orderRequest);
+            });
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, exception.ResponseCode);
+        }
+
+        [Test]
+        public void UpdateOrder_Successful()
+        {
+            var channelId = configuration["Payment:TestChannelId"];
+            var externalId = configuration["Payment:TestOrderExternalId"];
+            var order = service.GetOrder(channelId, externalId);
+            var newGuid = Guid.NewGuid();
+            var updateOrderRequest = new UpdateOrderRequest
+            {
+                Shopper = order.Shopper,
+                BillingAddress = order.BillingAddress,
+                Items = order.Items,
+                RiskData = order.RiskData
+            };
+            updateOrderRequest.Shopper.FirstName = $"Tom{newGuid}";
+            updateOrderRequest.BillingAddress.Line2 = $"Address{newGuid}";
+            updateOrderRequest.Items.ForEach(x => x.Name = $"Name{newGuid}");
+            updateOrderRequest.RiskData = new RiskData();
+
+            var updatedOrder = service.UpdateOrder(order.Id, updateOrderRequest);
+
+            AssertThatOrderIsCorrect(updatedOrder, channelId, externalId);
+            updatedOrder.ShouldBeEquivalentToObjectWithMoreProperties(order);
+        }
+
+        [Test]
+        public void UpdateOrder_IfDeliveryMethodIsInvalid_Exception400()
+        {
+            var channelId = configuration["Payment:TestChannelId"];
+            var externalId = configuration["Payment:TestOrderExternalId"];
+            var order = service.GetOrder(channelId, externalId);
+            var updateOrderRequest = new UpdateOrderRequest
+            {
+                Shopper = order.Shopper,
+                BillingAddress = order.BillingAddress,
+                Items = order.Items,
+                RiskData = new RiskData
+                {
+                    DaysToEvent = 2,
+                    DeliveryMethod = "invalid",
+                    OfficeId = 1
+                }
+            };
+
+            var exception = Assert.Catch<ApiException>(() =>
+            {
+                var updatedOrder = service.UpdateOrder(order.Id, updateOrderRequest);
+            });
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, exception.ResponseCode);
+        }
+
+        [Test]
+        public void UpdateOrder_IfOrderWithIdDoesNotExist_Exception404()
+        {
+            var orderId = "not_id";
+            var updateOrderRequest = new UpdateOrderRequest();
+
+            var exception = Assert.Catch<ApiException>(() =>
+            {
+                var updatedOrder = service.UpdateOrder(orderId, updateOrderRequest);
+            });
+
+            Assert.AreEqual(HttpStatusCode.NotFound, exception.ResponseCode);
+        }
+
+        private void AssertThatOrderIsCorrect(Order order, string channelId, string externalId)
+        {
+            Assert.IsNotNull(order.Id);
+            Assert.AreEqual(channelId, order.ChannelId);
+            Assert.AreEqual(externalId, order.ExternalId);
+        }
+    }
+}
