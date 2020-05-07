0d1c29523954e8af56cabbdd1f0bcba48ce105d6
Tatyana Evtushenko <tevtushenko@encore.co.uk>
2020-05-07 15:58:57 +0300

    APS-2461 Create models to call the checkout endpoint

---
+++ EncoreTickets.SDK/Checkout/Models/RequestModels/PaymentType.cs
@@ -0,0 +1,12 @@
+﻿namespace EncoreTickets.SDK.Checkout.Models.RequestModels
+{
+    public enum PaymentType
+    {
+        Card,
+        Amex,
+        Paypal,
+        Account,
+        Alipay,
+        Wechatpay
+    }
+}
