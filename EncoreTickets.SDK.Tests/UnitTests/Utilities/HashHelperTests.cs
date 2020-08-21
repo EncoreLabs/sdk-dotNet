85c6a28d936ef9b8a2a0e6d470ab7d75e196915f
Tatyana Evtushenko <tevtushenko@encore.co.uk>
2020-08-21 19:11:21 +0300

    12218 Create a helper for MD5 hashes

---
+++ EncoreTickets.SDK.Tests/UnitTests/Utilities/HashHelperTests.cs
@@ -0,0 +1,17 @@
+﻿using EncoreTickets.SDK.Utilities;
+using NUnit.Framework;
+
+namespace EncoreTickets.SDK.Tests.UnitTests.Utilities
+{
+    [TestFixture]
+    internal class HashHelperTests
+    {
+        [TestCase("24588862", "bf414cad7f3bb93106947d0268853018")]
+        public void CreateMd5Hash_ReturnsCorrectData(string source, string expected)
+        {
+            var actual = HashHelper.CreateMd5Hash(source);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ EncoreTickets.SDK/Utilities/HashHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EncoreTickets.SDK.Utilities
+{
+    public static class HashHelper
+    {
+        public static string CreateMd5Hash(string sourceData)
+        {
+            var md5 = MD5.Create();
+            var inputBytes = sourceData == null ? new byte[0] : Encoding.UTF8.GetBytes(sourceData);
+            var hashBytes = md5.ComputeHash(inputBytes);
+            return string.Concat(hashBytes.Select(x => x.ToString("x2")));
+        }
+    }
+}
