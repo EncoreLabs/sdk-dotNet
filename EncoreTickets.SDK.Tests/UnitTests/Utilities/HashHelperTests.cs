4deb788279206094de222df550094e349ecd1eab
Tatyana Evtushenko <tevtushenko@encore.co.uk>
2020-08-24 13:50:21 +0300

    12218 Fix hash generation for null strings

--- EncoreTickets.SDK.Tests/UnitTests/Utilities/HashHelperTests.cs
+++ EncoreTickets.SDK.Tests/UnitTests/Utilities/HashHelperTests.cs
@@ -7,6 +7,8 @@ namespace EncoreTickets.SDK.Tests.UnitTests.Utilities
     internal class HashHelperTests
     {
         [TestCase("24588862", "bf414cad7f3bb93106947d0268853018")]
+        [TestCase(null, null)]
+        [TestCase("", "d41d8cd98f00b204e9800998ecf8427e")]
         public void CreateMd5Hash_ReturnsCorrectData(string source, string expected)
         {
             var actual = HashHelper.CreateMd5Hash(source);
--- EncoreTickets.SDK/Utilities/HashHelper.cs
+++ EncoreTickets.SDK/Utilities/HashHelper.cs
@@ -8,8 +8,13 @@ namespace EncoreTickets.SDK.Utilities
     {
         public static string CreateMd5Hash(string sourceData)
         {
+            if (sourceData == null)
+            {
+                return null;
+            }
+
             var md5 = MD5.Create();
-            var inputBytes = sourceData == null ? new byte[0] : Encoding.UTF8.GetBytes(sourceData);
+            var inputBytes = Encoding.UTF8.GetBytes(sourceData);
             var hashBytes = md5.ComputeHash(inputBytes);
             return string.Concat(hashBytes.Select(x => x.ToString("x2")));
         }
